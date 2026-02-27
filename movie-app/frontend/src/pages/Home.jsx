34819e46243a534adb34d3efc984c1051def3f8f
Izel Ozhan <izelozhan@gmail.com>
2026-02-26 23:14:12 -0600

    home page update

---
+++ movie-app/frontend/src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1997" },
+    { id: 3, title: "Harry Potter", release_date: "2020" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies.."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => 
+          movie.title.toLowerCase().startsWith(searchQuery) && (
+            <MovieCard key={movie.id} movie={movie} />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
