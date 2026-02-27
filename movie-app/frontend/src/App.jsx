7524898c3bc824cd9e272557054e31e1a09be0a5
Izel Ozhan <izelozhan@gmail.com>
2026-02-26 23:35:35 -0600

    page routing and styling

--- movie-app/frontend/src/App.jsx
+++ movie-app/frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <Home />
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ movie-app/frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movie Yet</h2>
+      <p>Start adding movies to your favorites and they will appear</p>
+    </div>
+  )
+}
+
+export default Favorites;
